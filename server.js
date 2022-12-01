6c89577dd821e99e38bfeec33966f9f9dac7b111
Mahbubul Hasan <mahbubul.hasan@itgarage.net>
2022-12-01 23:22:47 +0600

    changed build path

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use(cors())
 app.use(express.static(path.join(__dirname,'/public')));
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
